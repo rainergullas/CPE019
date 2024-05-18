28cfceade544e2c2a687184e6b53a4759f64346b
rainergullas <157891110+rainergullas@users.noreply.github.com>
2024-05-18 15:43:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-import streamlit as st
-import numpy as np
+import os
+
+# Install OpenCV if not already installed
+streamlit_logger = os.popen('pip show opencv-python-headless').read()
+if "Version" not in streamlit_logger:
+    os.system("pip install opencv-python-headless")
+
+# Now you can import OpenCV
 import cv2
+import numpy as np
+import streamlit as st
 from tensorflow.keras.models import load_model
 
-
 # Load the trained model
-model_path = '/content/drive/MyDrive/Emtech Final Project/number_detection_model.h5'
+model_path = 'path_to_your_model.h5'
 model = load_model(model_path)
 
 # Define your Streamlit app
@@ -16,14 +23,16 @@ def main():
 
     if uploaded_image is not None:
         image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
-        grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-        resized_image = cv2.resize(grayscale_image, (28, 28))
+        resized_image = cv2.resize(image, (28, 28))
         normalized_image = resized_image.astype('float32') / 255.0
         reshaped_image = np.expand_dims(normalized_image, axis=0)
+        
         prediction = model.predict(reshaped_image)
         predicted_label = np.argmax(prediction)
+
         st.image(image, caption=f"Predicted Label: {predicted_label}", use_column_width=True)
 
 # Run the Streamlit app
 if __name__ == '__main__':
     main()
+
